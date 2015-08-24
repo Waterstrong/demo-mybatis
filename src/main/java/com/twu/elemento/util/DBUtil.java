4769690184ba429c05b5a50ca9417ef5eb2f2d23
Waterstrong <sqlin@thoughtworks.com>
2015-08-24 22:51:41 +0530

    [Waterstrong] - add database util to handle sql session

---
+++ src/main/java/com/twu/elemento/util/DBUtil.java
@@ -0,0 +1,26 @@
+package com.twu.elemento.util;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class DBUtil {
+    private static SqlSessionFactory factory;
+
+    static {
+        Reader reader;
+        try {
+            reader = Resources.getResourceAsReader("configuration.xml");
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        factory = new SqlSessionFactoryBuilder().build(reader);
+    }
+
+    public static SqlSessionFactory getSqlSessionFactory() {
+        return factory;
+    }
+}
